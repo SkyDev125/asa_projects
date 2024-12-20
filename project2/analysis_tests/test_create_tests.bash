ecf7fe0f2de27e7e2600fb8fdcd3a9233156f9f2
Sky <diogo_goncalves2004@hotmail.com>
2024-12-20 13:51:36 +0000

    more tests and outputs

--- project2/analysis_tests/test_create_tests.bash
+++ project2/analysis_tests/test_create_tests.bash
@@ -6,7 +6,7 @@
 mkdir -p le_growth
 
 # Create tests 1_1, 10_10, 20_20, 30_30 until 1000_1000
-for size in {1000..100000..1000}; do
+for size in {100..10000..100}; do
     # Number of stations, Number of connections, Number of lines, 1
-    /workspace/university/2nd_year/1st_sem/asa/asa_projects/project2/analysis_tests/p2_gerador 1000 $size $size 1 $size >le_growth/output_1000_${size}_${size}.in
+    /workspace/university/2nd_year/1st_sem/asa/asa_projects/project2/analysis_tests/p2_gerador 200 $size $size 1 $size >le_growth/output_200_${size}_${size}.in
 done
