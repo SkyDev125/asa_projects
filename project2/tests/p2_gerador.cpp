fb9a4239c3457f6092ced9ce9f1b1a15b8c251f4
Sky <diogo_goncalves2004@hotmail.com>
2024-12-20 12:32:43 +0000

    same growth added.

--- project2/tests/p2_gerador.cpp
+++ project2/tests/p2_gerador.cpp
@@ -3,31 +3,32 @@
 #include <set>
 #include <random>
 #include <algorithm>
+#include <numeric>
 using namespace std;
 
 // Returns a random value between [1, m]
 #define randomValue(m) ((rand() % m) + 1)
 
-//-----------------------------------------------------------------------------
+//----------------------------------------------------------------------------- 
 void printUsage(char *progname) {
   cerr << "Usage: " << progname << " <V> <E> <L> <b> <seed>" << endl;
   cerr << "  V: number of stations (vertices)" << endl;
   cerr << "  E: number of connections (edges) E >= V" << endl;
   cerr << "  L: number of lines (colors) L>1" << endl;
   cerr << "  b: all stations be connected? (optional, 0/1) E >= V" << endl;
-  cerr << "  seed: random seed generator (opcional)" << endl;
+  cerr << "  seed: random seed generator (optional)" << endl;
   exit(1);
 }
 
-//-----------------------------------------------------------------------------
+//----------------------------------------------------------------------------- 
 int main(int argc, char* argv[]) {
     int V, E, L;
     bool bConnect = false;
     int seed = 0;
 
     if (argc < 4 || argc > 6) {
-      cerr << "ERROR: Wrong number of arguments" << endl;
-      printUsage(argv[0]);
+        cerr << "ERROR: Wrong number of arguments" << endl;
+        printUsage(argv[0]);
     }
 
     sscanf(argv[1], "%d", &V);
@@ -35,106 +36,88 @@ int main(int argc, char* argv[]) {
     sscanf(argv[3], "%d", &L);
 
     if (argc > 4) {
-      int arg;
-      sscanf(argv[4], "%d", &arg);
-      bConnect = (arg!=0);
+        int arg;
+        sscanf(argv[4], "%d", &arg);
+        bConnect = (arg != 0);
     }
     if (argc > 5) {
-      sscanf(argv[5], "%d", &seed);
-      srand(seed);
-    } else { // pseudo-random seed
-      srand((unsigned int)time(NULL));
+        sscanf(argv[5], "%d", &seed);
     }
 
     if (L < 1) {
-      cerr << "ERROR: L < 1" << endl;
-      printUsage(argv[0]);
+        cerr << "ERROR: L < 1" << endl;
+        printUsage(argv[0]);
     }
-    if (bConnect && E < V) {
-      cerr << "ERROR: E < V and bConnect" << endl;
-      printUsage(argv[0]);
+    if (bConnect && E < V - 1) {
+        cerr << "ERROR: E < V-1 and bConnect" << endl;
+        printUsage(argv[0]);
     }
-    if (E > (V*L-V/2)) { // should be E>V*L but for quick convergence...
-      cerr << "ERROR: E > (V*L-V/2)" << endl;
-      printUsage(argv[0]);
+    if (E > V * L) {
+        cerr << "ERROR: E > V*L" << endl;
+        printUsage(argv[0]);
     }
 
-    //-------------------------------------------------------------------------
-    mt19937 rng(seed);
+    // Random number generator setup
+    mt19937 rng(seed ? seed : (unsigned int)time(NULL));
     vector<set<int>> lineStations(L);
-    // All stations
-    vector<int> vAllVs = vector<int>(V);
-    for (int i = 0; i < V; ++i) vAllVs[i] = i+1;
-    // All lines
-    vector<int> vAllLs = vector<int>(L);
-    for (int i = 0; i < L; ++i) vAllLs[i] = i;
+    vector<int> vAllVs(V);
+    iota(vAllVs.begin(), vAllVs.end(), 1);
+
+    vector<int> vAllLs(L);
+    iota(vAllLs.begin(), vAllLs.end(), 0);
     shuffle(vAllLs.begin(), vAllLs.end(), rng);
 
-    // Each line is contiguous
-    int lineEdges = V / L; // min # edges for a line
-    if (lineEdges < 1) lineEdges = 1;
-    for (int line = 0; line < L; line++) { // for each line
-        shuffle(vAllVs.begin(), vAllVs.end(), rng);
-        for (int i = 0; i < lineEdges; i++) { // add stations to line
-          lineStations[line].insert(vAllVs[i]);
+    vector<pair<int, int>> edges; // Store edges to print later
+
+    // If bConnect is true, generate a spanning tree
+    if (bConnect) {
+        vector<int> connected, unconnected(vAllVs);
+        shuffle(unconnected.begin(), unconnected.end(), rng);
+
+        connected.push_back(unconnected.back());
+        unconnected.pop_back();
+
+        while (!unconnected.empty()) {
+            int u = connected[rng() % connected.size()];
+            int v = unconnected.back();
+            unconnected.pop_back();
+
+            edges.emplace_back(u, v);
+            connected.push_back(v);
         }
     }
 
-    // force that all nodes are connected
-    if (bConnect) {
-      set<int> totalStations(vAllVs.begin(), vAllVs.end());
-      for (int l = 0; l < L; l++) {
-        for (const int e : lineStations[l])
-          totalStations.erase(e);
-      }
-      // totalStations has all stations not used yet
-      int lpos = 0;
-      while (!totalStations.empty()) {
-        int e = *totalStations.begin();
-        lineStations[lpos].insert(e);
-        totalStations.erase(e);
-        lpos = (lpos+1) % L; // round-robin on the lines
-      }
+    // Add remaining edges randomly
+    while ((int)edges.size() < E) {
+        int u = randomValue(V);
+        int v = randomValue(V);
+        if (u != v && find(edges.begin(), edges.end(), make_pair(u, v)) == edges.end() &&
+            find(edges.begin(), edges.end(), make_pair(v, u)) == edges.end()) {
+            edges.emplace_back(u, v);
+        }
     }
 
-    // Check current edges
-    int currE = 0;
-    for (int l = 0; l < L; ++l) { // |E| = |V| - 1
-      size_t sz = 0;
-      // it's not really an edge. Need to add a random station
-      while ((sz = lineStations[l].size()) == 1) {
-        lineStations[l].insert(randomValue(V));
-      }
-      currE += (sz - 1);
+    // Assign edges to lines dynamically as E and L grow
+    shuffle(edges.begin(), edges.end(), rng);
+    for (size_t i = 0; i < edges.size(); ++i) {
+        int line = vAllLs[i % L];
+        lineStations[line].insert(edges[i].first);
+        lineStations[line].insert(edges[i].second);
     }
 
-    // shuffle everything again, just because we can...
-    shuffle(vAllLs.begin(), vAllLs.end(), rng);
-    shuffle(vAllVs.begin(), vAllVs.end(), rng);
+    cout << V << " " << E << " " << L << endl;
 
-    // Do we need to add more edges?
-    int lpos = 0, spos = 0;
-    while (currE < E) {
-      size_t sz = lineStations[lpos].size();
-      lineStations[lpos].insert(vAllVs[spos]);
-      if (lineStations[lpos].size() > sz) currE++; // if it was added
-      lpos = (lpos+1) % L; // the round-robin way
-      spos = (spos+1) % V; // the round-robin way
+    // Print edges with line assignment
+    for (size_t i = 0; i < edges.size(); ++i) {
+        int line = (i % L) + 1;
+        cout << edges[i].first << " " << edges[i].second << " " << line << endl;
     }
 
-    cout << V << " " << currE << " " << L << endl;
-    // Print the edges
-    for (int l = 0; l < L; ++l) {
-      // linearly shuffle stations of line l
-      vector<int> stationList(lineStations[l].begin(), lineStations[l].end());
-      shuffle(stationList.begin(), stationList.end(), rng);
-      // print edge (e1, e2) -> l
-      for (size_t i = 1; i < stationList.size(); ++i) {
-        int e1 = stationList[i - 1];
-        int e2 = stationList[i];
-        cout << e1 << " " << e2 << " " << (l+1) << std::endl;
-      }
-    }
+    // Print stats for tests: constant V, varying E and L
+    cerr << "TEST SUMMARY:" << endl;
+    cerr << "Vertices: " << V << endl;
+    cerr << "Edges: " << E << endl;
+    cerr << "Lines: " << L << endl;
 
     return 0;
 }
