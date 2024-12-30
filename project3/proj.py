9c2e951ddd4e62f687fd796d4bee6ff4fd2cca85
Sky <diogo_goncalves2004@hotmail.com>
2024-12-30 07:59:22 +0000

    feat: refactored some of the code to make it less memory intensive.

--- project3/proj.py
+++ project3/proj.py
@@ -36,10 +36,10 @@ class Factory:
     def stock(self) -> int:
         return self._stock
 
-    def inner_orders(self) -> list:
+    def inner_orders(self) -> List[LpVariable]:
         return self._inner_orders
 
-    def outer_orders(self) -> list:
+    def outer_orders(self) -> List[LpVariable]:
         return self._outer_orders
 
     def __str__(self):
@@ -74,55 +74,48 @@ for i in range(1, factories + 1):
 
 # Create the LP problem
 prob = LpProblem("Gifts_received", LpMaximize)
-variables = {}
+variables = []
 
 # Fill Gift Wishes
 for i in range(factories + countries + 1, factories + countries + children + 1):
     line = input[i].split(" ")  # id, country, factories..
     child_id = line[0]
     country_id = line[1]
-    restriction = {}
+    restriction = []
 
     for j in range(2, len(line)):
         factory_id = line[j]
         # Create a variable id, factory, country for LP problem
-        restriction[child_id + factory_id] = LpVariable(
+        variable = LpVariable(
             f"x{child_id}_c{country_id}_f{factory_id}", 0, 1, cat="Binary"
         )
+        restriction.append(variable)
 
         # Add the order to the factory or country
         factory = (
             countries_list[int(country_id) - 1].factories().get(int(factory_id) - 1)
         )
         if factory:
-            factory.inner_orders().append(int(child_id) - 1)
+            factory.inner_orders().append(variable)
         else:
-            factories_list[int(factory_id) - 1].outer_orders().append(int(child_id) - 1)
+            factories_list[int(factory_id) - 1].outer_orders().append(variable)
 
-    prob += (lpSum(restriction.values()) <= 1, "Wishes of Child " + child_id)
-    variables.update(restriction)
+    prob += (lpSum(restriction) <= 1, "Wishes of Child " + child_id)
+    variables.extend(restriction)
+
+# Add the objective function
+prob += lpSum(variables), "Objective Function"
 
 # Input no longer needed
-del input, factories, countries, children
+del input, factories, countries, children, variables
 
 # Add the restrictions for the Factory stocks
 for factory_index, factory in enumerate(factories_list):
-    # Collect inner order variables
-    inner_orders_list = [
-        variables.get(str(child_id + 1) + str(factory_index + 1))
-        for child_id in factory.inner_orders()
-    ]
-
-    # Collect outer order variables
-    outer_orders_list = [
-        variables.get(str(child_id + 1) + str(factory_index + 1))
-        for child_id in factory.outer_orders()
-    ]
-
     # If there are any orders, add the "max stock" constraint
-    if inner_orders_list or outer_orders_list:
+    if (len(factory.outer_orders()) + len(factory.inner_orders())) > 0:
         prob += (
-            lpSum(inner_orders_list) + lpSum(outer_orders_list) <= factory.stock(),
+            lpSum(factory.outer_orders()) + lpSum(factory.inner_orders())
+            <= factory.stock(),
             "Max Stock of Factory " + str(factory_index + 1),
         )
 
@@ -130,16 +123,16 @@ for factory_index, factory in enumerate(factories_list):
 for country_index, country in enumerate(countries_list):
     # Collect outer order variables
     outer_orders_list = [
-        variables.get(str(child_id + 1) + str(factory_index + 1))
-        for factory_index, factory in country.factories().items()
-        for child_id in factory.outer_orders()
+        lpSum(factory.outer_orders())
+        for factory in country.factories().values()
+        if len(factory.outer_orders()) > 0
     ]
 
     # Collect inner order variables
     inner_orders_list = [
-        variables.get(str(child_id + 1) + str(factory_index + 1))
-        for factory_index, factory in country.factories().items()
-        for child_id in factory.inner_orders()
+        lpSum(factory.inner_orders())
+        for factory in country.factories().values()
+        if len(factory.inner_orders()) > 0
     ]
 
     # If there are any outer orders, add the "max export" constraint
@@ -156,8 +149,6 @@ for country_index, country in enumerate(countries_list):
             "Min Distribution of Country {}".format(country_index + 1),
         )
 
-# Add the objective function
-prob += lpSum(variables), "Objective Function"
 
 # # Print the Countries and Factories
 # for country_index, country in enumerate(countries_list):
@@ -170,11 +161,11 @@ prob += lpSum(variables), "Objective Function"
 #             + "------------------"
 #         )
 #         print(factory)
-#         print([child_id + 1 for child_id in factory.inner_orders()])
-#         print([child_id + 1 for child_id in factory.outer_orders()])
+#         print("Inner Orders: ", [v.name for v in factory.inner_orders()])
+#         print("Outer Orders: ", [v.name for v in factory.outer_orders()])
 
 # Remove the variables that are not used
-del variables, factories_list, countries_list
+del factories_list, countries_list
 
 # Solve the problem
 prob.solve(GLPK(msg=0))
